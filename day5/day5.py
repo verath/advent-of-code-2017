5faa73d4ca4ae4069a39d5ffa7057b61a96e2d22
Peter Eliasson <warackta@gmail.com>
2017-12-05 21:04:32 +0100

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,20 @@
+EXAMPLE_INPUT = "0\n3\n0\n1\n-3\n"
+
+def count_steps(jump_list):
+    jump_list = [int(n) for n in jump_list.splitlines()]
+    steps = 0
+    idx = 0
+    while idx < len(jump_list):
+        next_idx = idx + jump_list[idx]
+        jump_list[idx] += 1
+        idx = next_idx
+        steps += 1
+    return steps
+
+def main():
+    assert count_steps(EXAMPLE_INPUT) == 5
+    with open('input.txt', 'r') as input_file:
+        print(count_steps(input_file.read()))
+
+if __name__ == "__main__":
+    main()
