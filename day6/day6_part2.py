cccf518eb7b94f23a9eef94b2898bcbd54bf5863
Peter Eliasson <warackta@gmail.com>
2017-12-06 21:36:01 +0100

    Day6

---
+++ day6/day6_part2.py
@@ -0,0 +1,29 @@
+EXAMPLE_INPUT = "0	2	7	0"
+
+def count_inf_cycles(memBankStr):
+    history = []
+    mem_banks = [int(x) for x in memBankStr.strip().split()]
+    num_banks = len(mem_banks)
+    while True:
+        # Find largest
+        idx = mem_banks.index(max(mem_banks))
+        val = mem_banks[idx]
+        # Redistribute
+        mem_banks[idx] = 0
+        for _ in range(val):
+            idx += 1
+            if idx >= num_banks:
+                idx = 0
+            mem_banks[idx] += 1
+        # Test if we encountered this config before
+        if mem_banks in history:
+            return len(history) - history.index(mem_banks)
+        history.append(mem_banks[:])
+
+def main():
+    assert count_inf_cycles(EXAMPLE_INPUT) == 4
+    with open('input.txt', 'r') as input_file:
+        print(count_inf_cycles(input_file.read()))
+
+if __name__ == "__main__":
+    main()
